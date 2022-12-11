a6634ed1228597f0727e9eb8858f1590bed7dbe2
Wichapon <wichapon@gmail.com>
2022-12-11 16:28:25 +0700

    add loadNFT function with React

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../../assets/logo.png";
 import {HttpAgent} from "@dfinity/agent"
+import { idlFactory } from "../../../declarations/nft";
+import {Principal} from "@dfinity/principal"
 
 function Item(props) {
-  const id = props.id;
+  const id = Principal.fromText(props.id);
   const localHost = "http://127.0.0.1:8000/?canisterId=r7inp-6aaaa-aaaaa-aaabq-cai";
   const agent = new HttpAgent({host: localHost});
+
+  async function loadNFT(){
+    const NFTActor = await Actor.createActor(idlFactory,{
+      agent,
+      canisterId:id
+    });
+  }
+  //call only first time by using ,[]
+  useEffect(()=>{
+    loadNFT()
+  },[])
 
   return (
     <div className="disGrid-item">
