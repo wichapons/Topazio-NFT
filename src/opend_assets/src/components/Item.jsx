56b32de1ccba6776aad89b99157af4beb5fbba07
Wichapon <wichapon@gmail.com>
2022-12-11 17:09:32 +0700

    added NFT image URL converter

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -7,7 +7,10 @@ import {Principal} from "@dfinity/principal"
 
 function Item(props) {
   const [name,setName]= useState();
-  //const [owner,setOwner] = useState();
+  const [owner,setOwner] = useState();
+  const [image,setImage] = useState();
+
+
 
   const id = Principal.fromText(props.id);
   const localHost = "http://127.0.0.1:8000/?canisterId=r7inp-6aaaa-aaaaa-aaabq-cai";
@@ -19,10 +22,15 @@ function Item(props) {
       agent,
       canisterId:id
     });
-   // const ownerName = await NFTActor.getOwner();
+    const ownerName = await NFTActor.getOwner();
     const name = await NFTActor.getName();
-    //setOwner(ownerName);
+    const NFTimageData = await NFTActor.getAsset();
+    const NFTimage = new Uint8Array(NFTimageData);
+    const NFTimageURL =  URL.createObjectURL(new Blob ([NFTimage.buffer],{type: "image/png"}))
+
+    setOwner(ownerName.toText());
     setName(name);
+    setImage(NFTimageURL);
   };
 
   //call only first time by using ,[]
@@ -35,14 +43,14 @@ function Item(props) {
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={image}
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
             {name}<span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: 123
+            Owner: {owner}
           </p>
         </div>
       </div>
